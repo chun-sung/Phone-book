675a961b7713726ea780f78eab0cb614cb009272
chun-sung <springstar@daum.net>
2018-11-22 17:45:04 +0900

    finished

--- src/App.js
+++ src/App.js
@@ -5,59 +5,73 @@ import PhoneInfoList from './components/PhoneInfoList';
 class App extends Component {
   id = 2
   state = {
-    information:[
+    information: [
       {
         id: 0,
         name: '김민준',
         phone: '010-0000-0000'
       },
       {
-        id:1,
+        id: 1,
         name: '홍길동',
         phone: '010-0000-0001'
       }
-    ]
+    ],
+    keyword: ''
   }
-
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    });
+  }
   handleCreate = (data) => {
     const { information } = this.state;
     this.setState({
       information: information.concat({ id: this.id++, ...data })
     })
   }
-  
   handleRemove = (id) => {
     const { information } = this.state;
     this.setState({
       information: information.filter(info => info.id !== id)
     })
   }
-
   handleUpdate = (id, data) => {
     const { information } = this.state;
     this.setState({
       information: information.map(
         info => id === info.id
-          ? { ...info, ...data }
-          : info
+          ? { ...info, ...data } // 새 객체를 만들어서 기존의 값과 전달받은 data 을 덮어씀
+          : info // 기존의 값을 그대로 렌더링
       )
     })
   }
-
   render() {
-    const { information } = this.state;
+    const { information, keyword } = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
-         />
-         <PhoneInfoList 
-           data={information}
-           onRemove={this.handleRemove}
-           onUpdate={this.handleUpdate}
-         />
+        />
+        <p>
+          <input 
+            placeholder="검색 할 이름을 입력하세요.." 
+            onChange={this.handleChange}
+            value={keyword}
+          />
+        </p>
+        <hr />
+        <PhoneInfoList 
+          data={filteredList}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
+        />
       </div>
     );
   }
 }
+
 export default App;
